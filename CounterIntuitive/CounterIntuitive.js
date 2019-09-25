f3826a53a4e7eea9d197a101b57ab413a9db5125
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2019-09-25 23:07:03 +0530

    [master] Bug fix

--- CounterIntuitive/CounterIntuitive.js
+++ CounterIntuitive/CounterIntuitive.js
@@ -22,7 +22,10 @@ function solution(N, A) {
       dict[0] = ceil;
     }
     else{
-      if(!dict.hasOwnProperty(A[i])){ dict[A[i]] = ceil+1; }
+      if(!dict.hasOwnProperty(A[i])){
+        dict[A[i]] = dict[0] + 1;
+        if(dict[A[i]] > ceil) { ceil = dict[A[i]]; }
+      }
       else{
         if(dict[A[i]]+1 > ceil) { ceil = dict[A[i]] + 1; }
         dict[A[i]] = dict[A[i]]+1;
@@ -52,4 +55,4 @@ function solution(N, A) {
   return output;
 }
 
-console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]));
+console.log(solution(5, [3, 4, 4, 6, 1, 4, 4, 6, 1, 6, 1, 6, 1, 1, 6, 2, 3, 4]));
