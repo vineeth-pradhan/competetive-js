998bfbec12dd241dd03c206799d55b8e1b104a3c
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-12-03 10:36:56 +0530

    Fixing a bug & apply brute force method

--- MeetingSlots/MeetingSlots.js
+++ MeetingSlots/MeetingSlots.js
@@ -5,21 +5,40 @@ class MeetingSlots {
   }
 
 
+  // [[10, 50], [60, 70], [141, 150]],
+  // [[0, 15], [65, 120]],
   solution() {
     let firstAvailableSlot = [];
-    for(let i = 0; i < this.schedules[0].length; i++) {
-      if(this.schedules[0][i][1] - this.schedules[1][i][0] > this.minutes) {
-        firstAvailableSlot.push(this.schedules[1][i][0], this.schedules[0][i][0]+this.minutes);
-        return firstAvailableSlot;
+    let [shortestSlot, longestSlot] = this.reassignSlots(this.schedules[0], this.schedules[1]);
+    for(let i = 0; i < shortestSlot.length; i++) {
+      for(let j = 0; j < longestSlot.length; j++) {
+        if(shortestSlot[i][1] - shortestSlot[i][0] >= this.minutes && shortestSlot[i][1] - longestSlot[j][0] >= this.minutes && (longestSlot[j][1] - longestSlot[j][0] >= this.minutes) && longestSlot[j][1] - shortestSlot[i][0] >= this.minutes) {
+          let max = Math.max(shortestSlot[i][0], longestSlot[j][0])
+          firstAvailableSlot.push(max, max+this.minutes);
+        }
       }
     }
+    return firstAvailableSlot;
+  }
+
+  reassignSlots(firstSlot, secondSlot){
+    if(firstSlot.length > secondSlot.length) {
+      let tempSlot = firstSlot;
+      firstSlot = secondSlot;
+      secondSlot = tempSlot;
+    }
+    return [firstSlot, secondSlot];
   }
 }
 
 const meetingSlots = new MeetingSlots(
   [
-    [[0, 15], [60, 70], [140, 150]],
-    [[10, 50], [60, 120], [140, 210]]
+    [[0, 15], [60, 70], [141, 150]],
+    [[10, 50], [60, 120]],
+    // [[18, 26], [60, 120], [141, 150]],
+    // [[18, 30], [60, 70]],
+    // [[60, 70]],
+    // [[60, 120]],
   ],
   8
 )
